8b4f0f588beff38aec16858fbae456e1073364fb
Pedro Cardoso <www.pedrolucascardoso@gmail.com>
2023-08-31 13:17:56 -0300

    js implement

--- TELA TRIAGEM/TelaTriagem.js
+++ TELA TRIAGEM/TelaTriagem.js
@@ -1,20 +1,50 @@
 
 
 
+const frm = document.querySelector("form")
+const button = document.querySelector("#save");
 
-function formatarMoeda() {
-    var elemento = document.getElementById('valor');
-    var valor = elemento.value;
 
-    valor = valor + '';
-    valor = parseInt(valor.replace(/[\D]+/g, ''));
-    valor = valor + '';
-    valor = valor.replace(/([0-9]{2})$/g, ",$1");
 
-    if (valor.length > 6) {
-        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-    }
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
 
-    elemento.value = valor;
-    if (valor == 'NaN') elemento.value = '';
-}
+
+        
+            let listProcess = [];
+
+            const numberoProcess = document.querySelector("#ndoprocesso").value;
+            const dataChegada = document.querySelector("#datadapublicacao").value;
+            const recebida = document.querySelector("#caixa-recebido").value;
+            const tipoJustica = document.querySelector("#caixa-justica").value;
+            const autor = document.querySelector("#autor-text").value;
+            const situacaoJustica = document.querySelector("#caixa-liminar").value;
+            const obrigacaoFazer = document.querySelector("#obrigacao-fazer").value;
+            const valorMult = document.querySelector("#valor-mult").value;
+            const valorLimit = document.querySelector("#valor-limit").value;
+            const tipoMult = document.querySelector("#caixa-multa").value;
+            const determinacaoJud = document.querySelector("#caixa-jud").value;
+            const prazoFinal = document.querySelector("#caixa-prazo").value;
+
+            listProcess.push({
+                numberoProcess: numberoProcess,
+                dataChegada: dataChegada,
+                recebida: recebida,
+                tipoJustica: tipoJustica,
+                autor: autor,
+                situacaoJustica: situacaoJustica,
+                obrigacaoFazer: obrigacaoFazer,
+                valorMult: valorMult,
+                valorLimit: valorLimit,
+                tipoMult: tipoMult,
+                determinacaoJud: determinacaoJud,
+                prazoFinal: prazoFinal
+            });
+
+            console.log(listProcess);
+
+    });
+
+
+
+
