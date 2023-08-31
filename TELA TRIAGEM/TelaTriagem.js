f0048daa0239708802f77febce355d5e898341d7
Pedro Cardoso <www.pedrolucascardoso@gmail.com>
2023-08-31 13:44:51 -0300

    add sum

--- TELA TRIAGEM/TelaTriagem.js
+++ TELA TRIAGEM/TelaTriagem.js
@@ -3,48 +3,61 @@
 
 const frm = document.querySelector("form")
 const button = document.querySelector("#save");
+const text = document.querySelector("#text-date");
+
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
 
 
 
-    button.addEventListener("click", (e) => {
-        e.preventDefault();
+    let listProcess = [];
+
+    const numberoProcess = document.querySelector("#ndoprocesso").value;
+    const dataChegada = document.querySelector("#datadapublicacao").value;
+    const recebida = document.querySelector("#caixa-recebido").value;
+    const tipoJustica = document.querySelector("#caixa-justica").value;
+    const autor = document.querySelector("#autor-text").value;
+    const situacaoJustica = document.querySelector("#caixa-liminar").value;
+    const obrigacaoFazer = document.querySelector("#obrigacao-fazer").value;
+    const valorMult = document.querySelector("#valor-mult").value;
+    const valorLimit = document.querySelector("#valor-limit").value;
+    const tipoMult = document.querySelector("#caixa-multa").value;
+    const determinacaoJud = document.querySelector("#caixa-jud").value;
+    const prazoFinal = document.querySelector("#caixa-prazo").value;
+
+    let formul = {
+        numberoProcess: numberoProcess,
+        dataChegada: dataChegada,
+        recebida: recebida,
+        tipoJustica: tipoJustica,
+        autor: autor,
+        situacaoJustica: situacaoJustica,
+        obrigacaoFazer: obrigacaoFazer,
+        valorMult: valorMult,
+        valorLimit: valorLimit,
+        tipoMult: tipoMult,
+        determinacaoJud: determinacaoJud,
+        prazoFinal: prazoFinal
+    };
+
+    listProcess.push(formul);
 
 
-        
-            let listProcess = [];
+    console.log(formul);
+    console.log(listProcess);
 
-            const numberoProcess = document.querySelector("#ndoprocesso").value;
-            const dataChegada = document.querySelector("#datadapublicacao").value;
-            const recebida = document.querySelector("#caixa-recebido").value;
-            const tipoJustica = document.querySelector("#caixa-justica").value;
-            const autor = document.querySelector("#autor-text").value;
-            const situacaoJustica = document.querySelector("#caixa-liminar").value;
-            const obrigacaoFazer = document.querySelector("#obrigacao-fazer").value;
-            const valorMult = document.querySelector("#valor-mult").value;
-            const valorLimit = document.querySelector("#valor-limit").value;
-            const tipoMult = document.querySelector("#caixa-multa").value;
-            const determinacaoJud = document.querySelector("#caixa-jud").value;
-            const prazoFinal = document.querySelector("#caixa-prazo").value;
+});
 
-            listProcess.push({
-                numberoProcess: numberoProcess,
-                dataChegada: dataChegada,
-                recebida: recebida,
-                tipoJustica: tipoJustica,
-                autor: autor,
-                situacaoJustica: situacaoJustica,
-                obrigacaoFazer: obrigacaoFazer,
-                valorMult: valorMult,
-                valorLimit: valorLimit,
-                tipoMult: tipoMult,
-                determinacaoJud: determinacaoJud,
-                prazoFinal: prazoFinal
-            });
+function DateText() {
 
-            console.log(listProcess);
+    const dt = new Date();
 
-    });
+    text.innerHTML = dt;
+}
 
+
+DateText();
 
 
 
